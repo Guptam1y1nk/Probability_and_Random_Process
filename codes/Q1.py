202b99cbdb2a4cd67ffb9c1c3d9d38c6a5426821
Guptam1y1nk <141262935+Guptam1y1nk@users.noreply.github.com>
2023-08-07 22:53:21 +0530

    commit changes

--- codes/Q1.py
+++ codes/Q1.py
@@ -1,10 +1,5 @@
-import numpy as np
-
-#Defining the vectors A and B
-A = np.array([[1,1],[5,-7]])
-B = np.array([2,20])
-
-#solving Ax=B to find x
-x = np.linalg.solve(A,B)
-
-print(x)
+import numpy as np                       #Importing numpy
+A = np.array([[1,1],[5,-7]])             #Defining the vector A
+B = np.array([2,20])                     #Defining the vector B
+x = np.linalg.solve(A,B)                 #applying linalg.solve to find x such that Ax=B
+print(x)                                 #printing the solution of equation Ax=B 
