ab233eaf378d39758126b654d4c6e4be501cfe1e
Guptam1y1nk <m1y1nk2409@gmail.com>
2023-08-07 22:41:07 +0530

    restructure

--- codes/Q1.py
+++ codes/Q1.py
@@ -1,5 +1,5 @@
-import numpy as np
-A = np.array([[1,1],[5,-7]])
-B = np.array([2,20])
-x = np.linalg.solve(A,B)
-print(x)
+import numpy as np                       #Importing numpy
+A = np.array([[1,1],[5,-7]])             #Defining the vector A
+B = np.array([2,20])                     #Defining the vector B
+x = np.linalg.solve(A,B)                 #applying linalg.solve to find x such that Ax=B
+print(x)                                 #printing the solution of equation Ax=B 
