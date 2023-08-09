d67c78c5112606d96affa9716c14f75156860455
Guptam1y1nk <m1y1nk2409@gmail.com>
2023-08-10 00:16:33 +0530

    restructure

--- codes/Q1.py
+++ codes/Q1.py
@@ -1,50 +1,120 @@
 import numpy as np                       #Importing numpy
-<<<<<<< HEAD
 import matplotlib.pyplot as plt         
-=======
->>>>>>> 0f94fb650ee085f618cc7669b10c16dd22520923
 A = np.array([[1,1],[5,-7]])             #Defining the vector A
 B = np.array([2,20])                     #Defining the vector B
 x = np.linalg.solve(A,B)                 #applying linalg.solve to find x such that Ax=B
 print(x)                                 #printing the solution of equation Ax=B 
-<<<<<<< HEAD
 
 
-# Coordinates of the point
-x_point = 17/6
-y_point = -5/6
+#Orthogonal matrix
+omat = np.array([[0,1],[-1,0]]) 
 
-# Define the lines' equations
-x_line = np.linspace(-2, 3, 400)  # Generate x values from -2 to 3
-y_line = 2 - x_line
-y_equation = (5 * x_line - 20) / 7
+def dir_vec(A,B):
+  return B-A
+  
+def norm_vec(A,B):
+  return np.matmul(omat, dir_vec(A,B))
+  
+#Generate line points
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+  
+#Intersection of two lines
+def line_intersect(n1,A1,n2,A2):
+  N=np.vstack((n1,n2))
+  p = np.zeros(2)
+  p[0] = n1@A1
+  p[1] = n2@A2
+  #Intersection
+  P=np.linalg.inv(N)@p
+  return P
+  
+#Intersection of two lines
+def perp_foot(n,cn,P):
+  m = omat@n
+  N=np.block([[n],[m]])
+  p = np.zeros(2)
+  p[0] = cn
+  p[1] = m@P
+  #Intersection
+  x_0=np.linalg.inv(N)@p
+  return x_0
 
-# Create a new figure
-#plt.figure()
+#Triangle vertices
+def tri_vert(a,b,c):
+  p = (a**2 + c**2-b**2 )/(2*a)
+  q = np.sqrt(c**2-p**2)
+  A = np.array([p,q]) 
+  B = np.array([0,0]) 
+  C = np.array([a,0]) 
+  return  A,B,C
+  
+#Foot of the Altitude
+def alt_foot(A,B,C):
+  m = B-C
+  n = np.matmul(omat,m) 
+  N=np.vstack((m,n))
+  p = np.zeros(2)
+  p[0] = m@A 
+  p[1] = n@B
+  #Intersection
+  P=np.linalg.inv(N.T)@p
+  return P
 
-# Plot the point using a red 'x' marker
-plt.plot(x_point, y_point, 'bx', label='H (17/6, -5/6)')
+A = np.array([1,-1])
+B = np.array([-4, 6])
+C = np.array([-3,-5])
+H = np.array([17/6,-5/6])
 
-# Plot the lines
-plt.plot(x_line, y_line, label='y = 2 - x')
-plt.plot(x_line, y_equation, label='7y = 5x - 20')
+#Generating all lines
+x_AB = line_gen(A,B)	
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+x_AD = line_gen(A,alt_foot(A,B,C))
+x_AE = line_gen(A,alt_foot(B,A,C))
+x_BE = line_gen(B,alt_foot(B,A,C))
+x_CF = line_gen(C,alt_foot(C,A,B))
+x_AF = line_gen(A,alt_foot(C,A,B))
+x_CH = line_gen(C,H)
+x_BH = line_gen(B,H)
+x_AH = line_gen(A,H)
 
-# Set labels for the axes
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
+plt.plot(x_BE[0,:],x_BE[1,:],label='$BE$')
+plt.plot(x_AE[0,:],x_AE[1,:],linestyle = 'dashed',label='$AE$')
+plt.plot(x_CF[0,:],x_CF[1,:],label='$CF$')
+plt.plot(x_AF[0,:],x_AF[1,:],linestyle = 'dashed',label='$AF$')
+plt.plot(x_CH[0,:],x_CH[1,:],label='$CH$')
+plt.plot(x_BH[0,:],x_BH[1,:],label='$BH$')
+plt.plot(x_AH[0,:],x_AH[1,:],linestyle = 'dashed',label='$AH$')
 
-# Set the title of the plot
-plt.title('Plotting Point and Lines')
+#Labeling the coordinates
+tri_coords = np.vstack((A,B,C,alt_foot(A,B,C),alt_foot(B,A,C),alt_foot(C,A,B),H)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','D','E','F','H']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
 
-# Add a legend
-plt.legend()
-
-# Add text label "H" at the specified point
-plt.text(x_point, y_point, 'H', fontsize=12, verticalalignment='bottom')
-
-# Display the plot
-plt.grid(True)
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+plt.savefig('/home/mayank/tri_sss.png')
 plt.show()
 
-=======
->>>>>>> 0f94fb650ee085f618cc7669b10c16dd22520923
